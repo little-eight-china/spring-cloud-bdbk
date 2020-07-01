6f84772b02f90c869c4ed4ac499bfe8d8fec6b72
little-eight <295928126@qq.com>
2020-07-01 21:05:22 +0800

    feat: eureka的补充

--- bdbk-eureka/src/main/java/bdbk/BdbkEurekaApplication.java
+++ bdbk-eureka/src/main/java/bdbk/BdbkEurekaApplication.java
@@ -2,6 +2,8 @@ package bdbk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
